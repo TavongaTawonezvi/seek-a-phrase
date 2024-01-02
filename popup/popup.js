1fc79268e89f96e56912cd2df15a8b01ef266342
TavongaTawonezvi <ttawonezvi13@gmail.com>
2024-01-02 12:34:23 +0200

    add styles to popup and other extension functionality

--- popup/popup.js
+++ popup/popup.js
@@ -15,10 +15,10 @@ async function getTranscript() {
 	    const response = await fetch(url, options);
 	    const result = await response.json();
         
-        return result[0].transcription[0];
+        return result[0].transcription;
 
     } catch (error) {
-        console.error(error)
+       // console.error(error)
 	    return error;
     }
 
@@ -27,7 +27,45 @@ async function getTranscript() {
 
 async function main() {
     let transcription = await getTranscript();
-    console.log(transcription)
+
+     // Simulate an asynchronous operation (e.g., fetching data)
+    setTimeout(function() {
+        // Hide loading container
+        loadingContainer.style.display = 'none';
+    
+        // Show content container
+        contentContainer.style.display = 'block';
+    }, 600); // Adjust the delay as needed
+
+    let matches = [];
+
+    // find subs that match the search term
+    transcription.forEach(sub => {
+        if (sub['subtitle'].includes('function')) {
+            // clean subs
+            
+            matches.push({
+                subtitle: sub['subtitle'].replace(/\n/ , ' ').replace('&#39;', "'"),
+                start: sub['start']
+            })
+        }
+    });
+    console.log(matches)
 }
 
-main();
+let youtubeUrl;
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+     youtubeUrl = tabs[0].url;
+});
+console.log(youtubeUrl);
+
+var loadingContainer = document.querySelector('.loading-container');
+var contentContainer = document.querySelector('.popup-container');
+
+window.onload = function() {
+    // Show loading container
+    contentContainer.style.display = 'none';
+    loadingContainer.style.display = 'block';
+    main();
+
+};
